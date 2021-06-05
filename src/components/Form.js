ab41989c6f517aad77ef6b2e05cc7cb63d56ee21
jozef-wolf <wolf.joozef@gmail.com>
2021-06-05 14:24:48 +0200

    Form.js created and imported

---
+++ src/components/Form.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Form = () => {
+  return (
+    <div>
+      <form>
+        <label></label>
+        <input type="text"></input>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
