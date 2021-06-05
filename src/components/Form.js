9d00e936b77d498df59f44693131ebafce934f8c
jozef-wolf <wolf.joozef@gmail.com>
2021-06-05 14:33:12 +0200

    form structure wip

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,8 +4,16 @@ const Form = () => {
   return (
     <div>
       <form>
-        <label></label>
-        <input type="text"></input>
+        <label>Name</label>
+        <input type="text" required></input>
+        <label>Preperation Time</label>
+        <input type="time" step="2" required></input>
+        <label>Type</label>
+        <select>
+          <option>pizza</option>
+          <option>soup</option>
+          <option>sandwich</option>
+        </select>
       </form>
     </div>
   );
