342ef34325d2cd58daabd86e4fe2c770f1db8b04
jozef-wolf <wolf.joozef@gmail.com>
2021-06-05 19:16:19 +0200

    default select option

--- src/components/Form.js
+++ src/components/Form.js
@@ -21,6 +21,9 @@ const Form = () => {
         <input type="time" step="2" required></input>
         <label>Type</label>
         <select onChange={(e) => handleChang(e.target.value)}>
+          <option disabled selected value>
+            select dish
+          </option>
           {dishes.map((dish) => (
             <option key={dish.value} value={dish.value}>
               {dish.label}
