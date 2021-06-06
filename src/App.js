48822813c024643648073a2119cd37f56fb4b5da
jozef-wolf <wolf.joozef@gmail.com>
2021-06-06 22:40:48 +0200

    design wip

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import Form from "./components/Form";
+import styled from "styled-components";
+
+const Main = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+`;
 
 function App() {
   return (
-    <div className="App">
+    <Main>
       <Form />
-    </div>
+    </Main>
   );
 }
 
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,55 @@
 import React, { useState } from "react";
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 30vw;
+  padding: 10px 0 10px 0;
+  box-shadow: 2px 2px 8px 2px lightgray;
+  h2 {
+    font-size: 3rem;
+    letter-spacing: 10px;
+  }
+  form {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+    label {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background-color: #828282;
+      width: 30vw;
+      height: 40px;
+    }
+    input {
+      text-align: left;
+      margin: 5px;
+      border: 1px solid lightgray;
+      height: 20px;
+      width: 150px;
+    }
+    select {
+      border: none;
+      width: 150px;
+      height: 20px;
+      margin: 10px;
+    }
+  }
+  button {
+    height: 30px;
+    background-color: blue;
+    color: white;
+    border: 1px solid blue;
+    &:hover {
+      background-color: darkblue;
+    }
+  }
+`;
 
 const Form = () => {
   const [items] = React.useState([
@@ -45,7 +96,8 @@ const Form = () => {
   };
 
   return (
-    <div>
+    <StyledDiv>
+      <h2>FORM</h2>
       <form onSubmit={handleSubmit}>
         <label>Name</label>
         <input
@@ -116,9 +168,9 @@ const Form = () => {
           </>
         )}
 
-        <button>submit</button>
+        <button>Submit</button>
       </form>
-    </div>
+    </StyledDiv>
   );
 };
 
