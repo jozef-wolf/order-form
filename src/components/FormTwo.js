dacbd209d663840753fd310aeac13d714d038c03
jozef-wolf <wolf.joozef@gmail.com>
2021-06-06 21:20:20 +0200

    useState added to inputs

--- src/components/FormTwo.js
+++ src/components/FormTwo.js
@@ -9,9 +9,13 @@ const FormTwo = () => {
     { label: "Soup", value: "Soup" },
     { label: "Sandwich", value: "Sandwich" },
   ]);
-
+  const [name, setName] = useState("");
   const [type, setType] = useState([]);
-
+  const [preparationTime, setPreparationTime] = useState("");
+  const [pizzaSlices, setPizzaSlices] = useState("");
+  const [diameter, setdiameter] = useState("");
+  const [spiciness, setSpiciness] = useState("");
+  const [breadSlices, setbreadSlices] = useState("");
   const handleChange = (value) => {
     setType(value);
   };
@@ -19,9 +23,20 @@ const FormTwo = () => {
     <div>
       <form>
         <label>Name</label>
-        <input type="text" required></input>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        ></input>
         <label>Preparation Time</label>
-        <input type="time" step="2" required></input>
+        <input
+          type="time"
+          step="2"
+          value={preparationTime}
+          onChange={(e) => setPreparationTime(e.target.value)}
+          required
+        ></input>
         <label>Type</label>
         <select onChange={(e) => handleChange(e.target.value)}>
           <option defaultValue>select dish</option>
@@ -34,21 +49,45 @@ const FormTwo = () => {
         {type === "Pizza" && (
           <>
             <label>Number of slices</label>
-            <input type="number" required></input>
+            <input
+              type="number"
+              value={pizzaSlices}
+              onChange={(e) => setPizzaSlices(e.target.value)}
+              required
+            ></input>
             <label>Diameter</label>
-            <input type="number" step="0.01" required></input>
+            <input
+              type="number"
+              step="0.01"
+              value={diameter}
+              onChange={(e) => setdiameter(e.target.value)}
+              required
+            ></input>
           </>
         )}
         {type === "Soup" && (
           <>
             <label>Spiciness</label>
-            <input type="number" min="1" max="10" required></input>
+            <input
+              type="number"
+              min="1"
+              max="10"
+              value={spiciness}
+              onChange={(e) => setSpiciness(e.target.value)}
+              required
+            ></input>
           </>
         )}
         {type === "Sandwich" && (
           <>
             <label>Slices of bread</label>
-            <input type="number" min="1" required></input>
+            <input
+              type="number"
+              min="1"
+              value={breadSlices}
+              onChange={(e) => setbreadSlices(e.target.value)}
+              required
+            ></input>
           </>
         )}
 
